e893ceed8077be31a1cde9dc0b749cb2661b32d7
Fikerte-T <tofikertesfaye@gmail.com>
2022-06-01 18:09:59 +0300

    Setup model and add sample data

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,14 +1,5 @@
 class V1::GreetingsController < ApplicationController
     def index
-        render json: {:greetings => [
-            {
-                :name => 'something',
-                :guid => '129383sdhfkadbf'
-            },
-            {
-                :name => 'something else',
-                :guid => 'ftgsrtyw456346'
-            }
-        ] }.to_json
+
     end
 end
