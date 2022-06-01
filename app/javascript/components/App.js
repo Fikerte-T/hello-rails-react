a9ab0e2f290cdfb1a6dcee7d8643bd5fa939237f
Fikerte-T <tofikertesfaye@gmail.com>
2022-06-01 19:28:19 +0300

    Setup store, reducer, actions to fetch random greeting message from api

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,24 +1,21 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Greeting from './Greeting'
-// import { Provider } from "react-redux";
-// // import { store } from "../configureStore";
-// import configureStore  from "../configureStore";
-// const store = configureStore();
-// import store from "../configureStore";
-
+import { Provider } from "react-redux";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import store from "../configureStore";
+
 class App extends React.Component {
   render () {
     return (
-      // <Provider store={store}>
+      <Provider store={store}>
         <BrowserRouter>
           <Routes>
             <Route exact path="/" element={ "Home!" } />
-            <Route path="/greetings" element={ <Greeting greeting="friend"/>} />
+            <Route path="/greetings" element={ <Greeting />} />
           </Routes>
         </BrowserRouter>
-      // </Provider>
+      </Provider>
     );
   }
 }
